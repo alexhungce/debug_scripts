8dca6d2d6fe7b5ddaea242094976c447ecb1371a
Alex Hung <alex.hung@canonical.com>
2020-06-04 01:31:44 -0600

    acpidump: add scripts to generate acpidump from raw table data
    gen_acpidump.sh - used when no acpica-tools
    gen_acpidumpf.sh - using acpidump -f command
    
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

---
+++ gen_acpidump.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Copyright (C) 2020 Canonical
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+ACPI_DIR="/sys/firmware/acpi/tables"
+OUTPUT_FILE="$PWD/acpidump.log"
+
+if [ $EUID -ne 0 ]; then
+	echo "`basename $0`: must be executed with sudo"
+	exit 1
+fi
+
+if ! [ -z "$1" ]; then
+	ACPI_DIR=$1
+fi
+
+if ! [ -f $ACPI_DIR/DSDT ] ; then
+	echo "Not an ACPI table directory"
+	exit 1
+fi
+
+pushd . &> /dev/null
+
+cd $ACPI_DIR
+for table in * ; do
+	if [ -f $table ] ; then
+		echo ${table:0:4} @ 0x0000000000000000 >> $OUTPUT_FILE
+		xxd -u -c 16 -g 1 $table | sed 's/^0000/    /'  >> $OUTPUT_FILE
+		echo "" >> $OUTPUT_FILE
+	fi
+done
+
+[ -d dynamic ] || exit 2
+
+cd dynamic
+for table in * ; do
+	if [ -f $table ] ; then
+		echo ${table:0:4} @ 0x0000000000000000 >> $OUTPUT_FILE
+		xxd -u -c 16 -g 1 $table | sed 's/^0000/    /'  >> $OUTPUT_FILE
+		echo "" >> $OUTPUT_FILE
+	fi
+done
+
+# add permissions to non-root
+sudo chmod 666 $OUTPUT_FILE
+
+popd &> /dev/null
+
